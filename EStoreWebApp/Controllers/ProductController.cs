412332dacbdea6b000e7c830d84f46cd482d76cc
sachin.dwivedi <sachindwivedi1991@outlook.com>
2025-04-22 11:19:57 +0530

    Add ProductController with simple two actionmethods with set with required middleware

---
+++ EStoreWebApp/Controllers/ProductController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EStoreWebApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        [Route("/")]
+        public string Index()
+        {
+            return "List of Products";
+        }
+        [Route("create")]
+        public string Create()
+        {
+            return "Create Products";
+        }
+    }
+}
